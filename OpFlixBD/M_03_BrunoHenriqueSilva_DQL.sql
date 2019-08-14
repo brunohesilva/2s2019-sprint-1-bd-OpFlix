0ae887894ea1473fe2e648119093a863b2d01102
Bruno Henrique Silva <38940293827@info.local>
2019-08-14 08:18:08 -0300

    DQLFinal

--- OpFlixBD/M_03_BrunoHenriqueSilva_DQL.sql
+++ OpFlixBD/M_03_BrunoHenriqueSilva_DQL.sql
@@ -1,14 +1,10 @@
-USE M_OpFlix;
+USE M_OpFlixFinal;
 
 SELECT * FROM TipoUsuarios;
 SELECT * FROM Usuarios;
 SELECT * FROM Categorias;
-SELECT * FROM FilmesSeries;
 SELECT * FROM TipoFilmesSeries;
 SELECT * FROM MeiosVeiculacao;
+SELECT * FROM FilmesSeries;
 SELECT * FROM OndeLanca;
 
-SELECT TU.* , U.*
-FROM TipoUsuarios TU
-JOIN Usuarios U
-ON TU.IdTipoUsuario = U.IdTipoUsuario;
